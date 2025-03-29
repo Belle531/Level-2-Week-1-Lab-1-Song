68db4c5d6c1f2afb64cd244a7f6d611d06e75565
Cassandra <cassandra.moore1516@gmail.com>
2025-03-29 10:28:51 -0500

    Fixed code errors

---
+++ script.js
@@ -0,0 +1,20 @@
+function refrain() {
+    console.log("Code, Code, everywhere if you dare!");
+    console.log("Type it fast as you can, if you dare!");
+}
+
+function printedSong() {
+    
+    refrain();
+    console.log("Buggy lines? Oh, beware of the buggy lines!");
+    console.log("Fix them here, fix them there, fix them everywhere!");
+    refrain();
+    console.log("Loops that run but never end, catch me if you can!");
+    console.log("You must mend, mend, and fix, logic broken fast as you can!");
+    console.log("Compile, debug, try it once more, as fast as you can!");
+    console.log("Type it fast as you can, if you dare!");
+    console.log("Hello, console!")
+}
+
+
+printedSong();
